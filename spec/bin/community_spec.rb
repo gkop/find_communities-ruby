e1d58ee2a27dc5ca814cf7e724bceef1bccb60e6
Gabe Kopley <gabe@coshx.com>
2013-04-15 14:43:03 -0700

    Accept params in any order.  Test for verbose mode.

--- spec/bin/community_spec.rb
+++ spec/bin/community_spec.rb
@@ -3,13 +3,37 @@ require 'spec_helper'
 describe "community (executable)" do
   let(:filename) { File.dirname(__FILE__) + "/../data/karate.bin" }
 
-  it "outputs a hierarchy", :retry => 2 do
-    run "community -l -1 #{filename}"
-    relevant_lines = out.split("\n").last(4)
-    relevant_lines[0].should == "1 1"
-    relevant_lines[1].should == "2 2"
-    relevant_lines[2].should == "3 3"
-    relevant_lines[3].to_f.round(2).should == 0.43
+  context "outputs a hierarchy", :retry => 2 do
+    it "works when no level given" do
+      run "community #{filename}"
+      relevant_lines = out.split("\n").last(4)
+      3.times do |i|
+        relevant_lines[i].should == "#{i+1} #{i+1}"
+      end
+      relevant_lines[3].to_f.round(2).should == 0.43
+    end
+
+    it "works when level given is -1" do
+      run "community -l -1 #{filename}"
+      relevant_lines = out.split("\n").last(4)
+      3.times do |i|
+        relevant_lines[i].should == "#{i+1} #{i+1}"
+      end
+      relevant_lines[3].to_f.round(2).should == 0.43
+    end
+
+    it "can provide verbose output", :retry => 2 do
+      run "community #{filename} -v"
+      output = out
+      3.times do |i|
+        output.should match /^level #{i}:\n  start computation [0-9\-: ]+\n  network size: \d+ nodes, \d+ links, \d+ weight.$(\n\d+ \d+){4,}\n  modularity increased from (-)?\d+\.\d+ to \d+\.\d+\n  end computation [0-9\-: ]+$/
+      end
+      lines = output.split("\n")
+      lines.first.should match /^Begin: [0-9\-: ]+$/
+      lines[lines.length-3].should match /^End: [0-9\-: ]+$/
+      lines[lines.length-2].should match /^Total duration: \d+\.\d+ sec\.$/
+      lines.last.to_f.round(2).should == 0.43
+    end
   end
 
   it "outputs level 2 in the hierchy", :retry => 2 do
@@ -23,7 +47,7 @@ describe "community (executable)" do
   end
 
   it "outputs level 3 in the hierchy", :retry => 2 do
-    run "community -l 3 #{filename}"
+    run "community #{filename} -l 3"
     lines = out.split("\n")
     lines.length.should == 5
     lines[0..lines.length-2].each_with_index do |l, i|
